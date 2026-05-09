c2c5615aafba91efb4acd9a1e0c656ed260ea9ff
beelz3bub <beelz33u3@gmail.com>
2026-05-10 01:10:20 +0700

    fix: use catch-all route instead of rewrites and replace bcrypt with bcryptjs

--- api/[...path].js
+++ api/[...path].js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./database');
 const nodemailer = require('nodemailer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'rahmatfix-super-secret-key-2026';
